acf04e6f7b12842f437e357d01eb763ef4d46718
josepCortesManyanich <gaara96@gmail.com>
2024-05-21 16:17:59 +0200

    solving

--- jquery.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('#Products').on('click','.add-to', function(){
-        let productName = $(this).closest('.product-card').data('name');
-        $('.cart-list').append('<li>'+ productName + '</li>')
-    })
-})
